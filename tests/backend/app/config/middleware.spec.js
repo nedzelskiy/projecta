7f8ce55f542b2c20208dbeef6248bf9eb286a05f
Pavel <pavel@fironet.com.ua>
2016-08-31 15:04:20 +0300

    minor upgrades

--- tests/backend/app/config/middleware.spec.js
+++ tests/backend/app/config/middleware.spec.js
@@ -1,32 +1,40 @@
 //var util = require('util');
 describe('Middleware setup', function() {
+
   var app = require('../../../../app/config/middleware');
+
+  var returnLayer = function(layerName) {
+    return app._router.stack.filter(function(layer) {
+      return layer && layer.handle && layer.handle.name === layerName;
+    });
+  };
+
   it('should load jsonParser', function(next) {
     // use(bodyParser.json());
-    expect((app._router.stack.filter(function(layer) {
-      return layer && layer.handle && layer.handle.name === 'jsonParser';
-    })).length > 0).toBe(true);
+    var layer = returnLayer('jsonParser');
+    expect(layer.length === 1).toBe(true);
     next();
   });
+
   it('should load urlencodedParser', function(next) {
     // use.(bodyParser.urlencoded({ extended: true })
-    expect((app._router.stack.filter(function(layer) {
-      return layer && layer.handle && layer.handle.name === 'urlencodedParser';
-    })).length > 0).toBe(true);
+    var layer = returnLayer('urlencodedParser');
+    expect(layer.length === 1).toBe(true);
     next();
   });
+
   it('should load router', function(next) {
     // app.use(require('./routes'));
-    expect((app._router.stack.filter(function(layer) {
-      return layer && layer.handle && layer.handle.name === 'router';
-    })).length > 0).toBe(true);
+    var layer = returnLayer('router');
+    expect(layer.length === 1).toBe(true);
     next();
   });
+
   it('should load static', function(next) {
     // app.use(express.static(...));
-    expect((app._router.stack.filter(function(layer) {
-      return layer && layer.handle && layer.handle.name === 'serveStatic';
-    })).length === 3).toBe(true);
+    var layer = returnLayer('serveStatic');
+    expect(layer.length === 3).toBe(true);
     next();
   });
+
 });
