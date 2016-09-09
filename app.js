e81c436e429724efa8331911df951135bdae4784
Pavel <pavel@fironet.com.ua>
2016-09-09 11:28:11 +0300

    test

--- app.js
+++ app.js
@@ -7,5 +7,6 @@ var server = app.listen(config.server.port);
 console.log('Env = ' + config.mode);
 console.log('Example app listening on port %s [%s]', server.address().port, new Date());
 
+
 app.set('views', config.server.wayToViews);
 app.set('view engine', config.server.viewsFormat);
